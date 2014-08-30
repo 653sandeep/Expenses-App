93bccec3f4cb894a461fc92079eee278e8dbeb0f
sandeep <653sandeep@gmail.com>
2014-08-30 23:59:59 +0530

    removed comments

--- app.js
+++ app.js
@@ -1,25 +1,14 @@
 var express = require('express'), 
     http = require('http');
-    //path = require('path'),
     Cash = require('/Users/sandeep/expressDir/ExpensesApp/Cash');
-
 var bodyParser = require('body-parser');
-
-
 var app = express();
-    //app.set('port', process.env.PORT || 6000);
-    //app.set('views', __dirname + '/views');
-    //app.set('view engine', 'ejs');
-    //app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.set('views', __dirname + '/views');
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'ejs');
 
-    app.use(bodyParser.urlencoded({ extended: false }));
-    app.use(bodyParser.json());
-
-    app.set('views', __dirname + '/views');
-    app.engine('html', require('ejs').renderFile);
-    app.set('view engine', 'ejs');
-
-// Render our home page with all blog posts
 app.get('/', function(request, response) {
     Cash.find(function(err, cash) {
         if (err) {
@@ -35,14 +24,11 @@ app.get('/', function(request, response) {
     });
 });
 
-// Render a form to enter a new post
 app.get('/new', function(request, response) {
     response.render('new', {});
 });
 
-// create a new blog post object
 app.post('/create', function(request, response) {
-    //Create and save a Post model
     var cash = new Cash({
         date            : request.body.date,
         amount          : request.body.amount,
@@ -51,7 +37,6 @@ app.post('/create', function(request, response) {
         remainingAmount : request.body.remainingAmount
     });
 
-    //Save the model
     cash.save(function(err, model) {
         if (err) {
             response.send(500, 'There was an error ');
